6806ce47cfeab94b3af47b81f63e0bc953e5300d
Ivor Ćelić <ivorcelic@gmail.com>
2019-11-27 22:11:05 +0100

    proba

--- VSC/DBeaver_SpajanjeTablica/Mamineslastice_spajanje.sql
+++ VSC/DBeaver_SpajanjeTablica/Mamineslastice_spajanje.sql
@@ -28,3 +28,40 @@ inner join kategorija b on a.kategorija = b.kategorija_ID
 inner join recept_namirnica c on a.recept_ID = c.recept
 inner join sastojak d on c.recept = d.sastojak_ID
 where a.naziv='Francuska salata';
+
+
+#________________________________________________________________________#
+#________________________________________________________________________#
+#________________________________________________________________________#
+
+
+select * from kategorija;
+select * from recept;
+select * from sastojak;
+
+select * 
+
+# ispisati sastojke za teletinu, kojoj kategoriji pripada i komentare vezane za taj recept 
+select a.naziv as recept, a.kategorija, c.sastojak, e.komentar_text as komentar
+from recept a
+inner join kategorija b on a.kategorija = b.kategorija_ID
+inner join recept_sastojak c on a.recept_ID = c.recept
+inner join sastojak d on c.sastojak = d.sastojak_ID
+inner join komentar e on e.recept = a.recept_ID;
+
+
+# izlistajte sve kategorije koje u počinju sa slovom p
+select * from kategorija where naziv like 'p%';
+
+# izlistajte sve kategorije koje u sebi imaju slovo m ili t
+select * from kategorija where naziv like '%t%'
+	or naziv like '%m%';
+	
+# izlistajte sve sastojke koji se ne koriste ni za jedan recept
+select b.sastojak
+from recept a
+left join recept_sastojak b on a.recept_ID = b.recept
+left join sastojak c on c.sastojak_ID = b.sastojak
+where b.recept is null;
+
+select * from recept_sastojak;
