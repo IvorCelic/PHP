fb564d8aad257e03b1489fc8e3b0ae5568cbe345
Ivor Ćelić <ivorcelic@gmail.com>
2023-12-08 08:59:57 +0100

    Reorganizacija

---
+++ predavac01.edunova.hr/controller/IndexController.php
@@ -0,0 +1,41 @@
+<?php
+
+class IndexController
+{
+
+    private $view;
+
+    public function __construct()
+    {
+        $this->view = new View();
+    }
+
+    public function index()
+    {
+        $poruka='hello iz kontrolera';
+        $kod=22;
+
+       
+        $this->view->render('pocetna',[
+            'p'=>$poruka,
+            'k'=>$kod]
+        );
+
+
+    }
+    public function onama()
+    {
+        $this->view->render('onama');
+    }
+
+    public function json()
+    {
+        $niz=[];
+        $s=new stdClass();
+        $s->naziv='PHP programiranje';
+        $s->sifra=1;
+        $niz[]=$s;
+        //$this->view->render('onama',$niz);
+        echo json_encode($niz);
+    }
+}
---
+++ predavac01.edunova.hr/konfiguracija.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'nazivAPP' => 'Edunova APP',
+    'url' => 'http://predavac01.edunova.hr/',
+    'dev' => true
+];
